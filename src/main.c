0d9061577fdcf702971a080354b4978cc9d5c57a
Nicerice96 <100174834+Nicerice96@users.noreply.github.com>
2026-02-06 21:48:55 -0500

    building internal shell logic

--- src/main.c
+++ src/main.c
@@ -16,8 +16,32 @@ void shellLoop(char **line, size_t *len){
             break;
         }
 
+        pid_t pid = fork();
 
-        //Forking part which I don't understand yet...
+        if(pid == 0){
+            //CHILD PROCESS : Safe to execute here
+            //This process will be replaced by the command
+
+            line[strcspn(line, "\n")] = '\0';
+
+            if(strcmp(line, "cd") == 0){
+                //handle cd logic
+            }
+            else if(strcmp(line, "ls") == 0){
+                //handle ls logic
+            }
+            else if(strcmp(line, "exit") == 0){
+                break;
+            }
+            else{
+                printf("ERR: Unknown command!");
+            }
+        }
+        else{
+            //PARENT PROCESS
+
+            wait(NULL);
+        }
     }
 
 }
